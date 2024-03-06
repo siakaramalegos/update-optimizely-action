5b8914b247d6a25896acf6abd25e6c2995b7dea3
Sia Karamalegos <siakaramalegos@gmail.com>
2024-03-06 16:03:55 -0500

    Stream file

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs')
+// const { mkdir } = require("fs/promises");
+const { Readable } = require('stream');
+const { finished } = require('stream/promises');
+const path = require("path");
+
+const downloadFile = (async (url, fileName) => {
+  const res = await fetch(url);
+  // if (!fs.existsSync("downloads")) await mkdir("downloads"); //Optional if you already have downloads directory
+  // const destination = path.resolve("./downloads", fileName);
+  // const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
+  const fileStream = fs.createWriteStream(fileName, { flags: 'wx' });
+  await finished(Readable.fromWeb(res.body).pipe(fileStream));
+});
+
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
@@ -11,13 +25,10 @@ try {
   const url = core.getInput('url');
   const filename = core.getInput('filename')
   console.log(`Fetching ${url}!`);
-  fetch(url)
-    .then(response => response.blob())
-    .then(blob => {
-      fs.writeFileSync(filename, blob)
-      core.setOutput("status", "OK");
-      console.log(`${filename} saved!`)
-    })
+
+  await downloadFile(url, filename)
+  core.setOutput("status", "OK");
+  console.log(`${filename} saved!`)
   
 } catch (error) {
   core.setFailed(error.message);
