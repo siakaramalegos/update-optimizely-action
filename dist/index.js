d36b6c3a103ee0c53af36e736095613a78527611
Sia Karamalegos <siakaramalegos@gmail.com>
2024-03-06 15:41:14 -0500

    Fetches a file

--- dist/index.js
+++ dist/index.js
@@ -30864,16 +30864,25 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(4181);
 const github = __nccwpck_require__(2726);
+const fs = __nccwpck_require__(7147)
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  // `url` input defined in action metadata file
+  const url = core.getInput('url');
+  const filename = core.getInput('filename')
+  console.log(`Fetching ${url}!`);
+  fetch(url)
+    .then(response => response.blob())
+    .then(blob => {
+      fs.writeFileSync(filename, blob)
+      core.setOutput("status", "OK");
+      console.log(`${filename} saved!`)
+    })
+  
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs')
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  // `url` input defined in action metadata file
+  const url = core.getInput('url');
+  const filename = core.getInput('filename')
+  console.log(`Fetching ${url}!`);
+  fetch(url)
+    .then(response => response.blob())
+    .then(blob => {
+      fs.writeFileSync(filename, blob)
+      core.setOutput("status", "OK");
+      console.log(`${filename} saved!`)
+    })
+  
 } catch (error) {
   core.setFailed(error.message);
 }
