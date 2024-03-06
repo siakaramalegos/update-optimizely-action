553ac4288e005123e9fb694a7fe35312d95dd80a
Sia Karamalegos <siakaramalegos@gmail.com>
2024-03-06 16:12:43 -0500

    Trying different readable stream

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const fs = require('fs')
 // const { mkdir } = require("fs/promises");
 const { Readable } = require('stream');
 const { finished } = require('stream/promises');
-const path = require("path");
+// const path = require("path");
 
-const downloadFile = (async (url, fileName) => {
-  const res = await fetch(url);
-  // if (!fs.existsSync("downloads")) await mkdir("downloads"); //Optional if you already have downloads directory
-  // const destination = path.resolve("./downloads", fileName);
-  // const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
-  const fileStream = fs.createWriteStream(fileName, { flags: 'wx' });
-  await finished(Readable.fromWeb(res.body).pipe(fileStream));
-});
+// const downloadFile = (async (url, fileName) => {
+//   const res = await fetch(url);
+//   // if (!fs.existsSync("downloads")) await mkdir("downloads"); //Optional if you already have downloads directory
+//   // const destination = path.resolve("./downloads", fileName);
+//   // const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
+//   const fileStream = fs.createWriteStream(fileName, { flags: 'wx' });
+//   await finished(Readable.fromWeb(res.body).pipe(fileStream));
+// });
 
 
 try {
@@ -26,7 +26,11 @@ try {
   const filename = core.getInput('filename')
   console.log(`Fetching ${url}!`);
 
-  await downloadFile(url, filename)
+
+  const stream = fs.createWriteStream(filename);
+  const { body } = await fetch(url);
+  await finished(Readable.fromWeb(body).pipe(stream));
+
   core.setOutput("status", "OK");
   console.log(`${filename} saved!`)
   
